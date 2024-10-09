9416a52bd7955af6bfd8d1b23c742d6b73d74871
vannavolgaa <140949773+vannavolgaa@users.noreply.github.com>
2024-10-09 15:39:55 +0200

    Update parameters.py

--- src/parameters.py
+++ src/parameters.py
@@ -5,9 +5,5 @@ from datetime import timedelta
 class BacktestParameters: 
     csv_lines_to_load: int = 500000
 
-    garch_time_delta: timedelta = timedelta(days = 10)
-
-    auto_regressive_time_delta: timedelta = timedelta(days = 10)
-
 
 
